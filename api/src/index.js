3fbe88aebbf1a16b0fdf0df65b9c04a4644dd239
Francesco Fera <francescofera157@gmail.com>
2025-04-16 17:46:13 +0200

    feature: created basic notification routes

--- api/src/index.js
+++ api/src/index.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.use("/api/notifications", notificationRoutes);
+
 app.listen(3000, () => {
   console.log("Notification Api running on port 3000");
 });
--- api/src/routes/notifications.js
+++ api/src/routes/notifications.js
@@ -1,7 +1,23 @@
 import { z } from "zod";
 import express from "express";
 import Redis from "ioredis";
+import { nanoid } from "nanoid";
 
 const REDIS_URI = process.env.REDIS_URI;
+
 const router = express.Router();
-const redis = new Redis();
+const redis = new Redis(REDIS_URI);
+
+router.post("/", async (req, redis) => {
+  try {
+    const data = req.body;
+    const id = nanoid();
+
+    await redis.lpush("notificationsQueue", JSON.stringify({ id, ...data }));
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+export default router;
