cc97474b9b6036476a87ad6f9f0826adefb65f7d
Francesco Fera <francescofera157@gmail.com>
2025-04-16 23:00:18 +0200

    added timestamps on notification model

--- processor/src/models/notification.js
+++ processor/src/models/notification.js
@@ -2,15 +2,18 @@ import mongoose from "mongoose";
 
 const Notification = mongoose.model(
   "Notification",
-  new mongoose.Schema({
-    id: String,
-    type: String,
-    to: String,
-    message: String,
-    status: String,
-    attempts: Number,
-    lastError: String,
-  })
+  new mongoose.Schema(
+    {
+      id: String,
+      type: String,
+      to: String,
+      message: String,
+      status: String,
+      attempts: Number,
+      lastError: String,
+    },
+    { timestamps: true }
+  )
 );
 
 export default Notification;
